7d43ab4a0c8af8a0f979690ffd813974b4df4be3
Mapaor4 <mpardoor24@alumnes.ub.edu>
2026-01-16 17:25:35 +0100

    minor ui fix

--- src/components/terminal/utils.ts
+++ src/components/terminal/utils.ts
@@ -6,7 +6,7 @@ export const writePrompt = (term: Terminal, cwd: string) => {
 
 export const writeWelcomeMessage = (term: Terminal) => {
   term.writeln('\x1b[1;38;5;120m╭─ Welcome to Git Online Learning Terminal! ─╮\x1b[0m');
-  term.writeln('\x1b[38;5;250m│ Type "help" for available commands          │\x1b[0m');
-  term.writeln('\x1b[38;5;250m│ Type "git" for git commands                 │\x1b[0m');
-  term.writeln('\x1b[1;38;5;120m╰──────────────────────────────────────────────╯\x1b[0m\n');
+  term.writeln('\x1b[38;5;250m│ Type "help" for available commands         │\x1b[0m');
+  term.writeln('\x1b[38;5;250m│ Type "git" for git commands                │\x1b[0m');
+  term.writeln('\x1b[1;38;5;120m╰────────────────────────────────────────────╯\x1b[0m\n');
 };
